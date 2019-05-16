ff373a34124c69b80488d7f281a500a67312df07
Peter_清风 <865268033@qq.com>
2019-05-16 11:21:54 +0800

    Add files via upload

---
+++ Model.py
@@ -0,0 +1,20 @@
+"""-*- coding: utf-8 -*-
+ DateTime   : 2019/5/16 11:15
+ Author  : Peter_Bonnie
+ FileName    : Model
+ Software: PyCharm
+"""
+class Model(object):
+
+    def __init__(self):
+
+        """主要有模型预测的结果，损失函数..."""
+        self.prediction,self.loss='',''
+
+
+    def run_model(self):
+        pass
+
+    def evluation_model(self):
+        pass
+
